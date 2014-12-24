0829a01733fca3c1d54c9da34051829963c47fe6
Gomosoft <jgomez@soluntech.com>
2014-12-24 13:32:11 -0500

    changes

--- admin/rest/Connection.php
+++ admin/rest/Connection.php
@@ -2,8 +2,8 @@
 	Class Connection{
 		public function connection(){
 		    $dbhost="localhost";
-		    $dbuser="root";
-		    $dbpass="";
+		    $dbuser="4nU53r";
+		    $dbpass="G0m0$0f7_2014";
 		    $dbname="broadcast";
 		    $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);  
 		    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
