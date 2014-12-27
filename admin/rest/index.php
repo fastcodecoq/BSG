99848943e682a19e7913f571c30c88316847b5df
Gomosoft <jgomez@soluntech.com>
2014-12-27 14:00:09 -0500

    changes

--- admin/rest/index.php
+++ admin/rest/index.php
@@ -12,6 +12,24 @@ $brand = new Brand();
 $user = new User();
 $company = new Company();
 
+// Allow from any origin
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");         
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    }
+    
+
 $app->post('/login', function() use($user, $app) {
     $user->login();
 });
